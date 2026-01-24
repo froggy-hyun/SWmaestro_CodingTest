78bf82d630dec74ff1d41dc41261f34e055dbe1a
Evan Choi (최성현) <111727754+froggy-hyun@users.noreply.github.com>
2026-01-25 02:13:21 +0900

    [level 2] Title: 기능개발, Time: 0.77 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,20 +1,16 @@
 def solution(progresses, speeds):
     answer = []
     while(progresses):
-        # 작업 수행
+        count = 0
         for i in range(len(progresses)):
-            if(progresses[i] < 100):
-                progresses[i] += speeds[i]
+            progresses[i] += speeds[i]
         
-        # 배포 가능 여부 체크 및 배포
-        release = 0
-        while(len(progresses) != 0):
-            if(progresses[0] >= 100):
+        for i in range(len(progresses)):      
+            if progresses[0] >= 100:
+                count += 1
                 progresses.remove(progresses[0])
                 speeds.remove(speeds[0])
-                release += 1
-            else:
-                break
-        if(release != 0):
-            answer.append(release)
+        if count != 0:
+            answer.append(count)
+                    
     return answer
