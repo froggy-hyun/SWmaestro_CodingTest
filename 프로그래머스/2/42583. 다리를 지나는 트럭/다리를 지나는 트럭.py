f9c606ce95bc3a40e5b9125b9f12d48606797833
Evan Choi (최성현) <111727754+froggy-hyun@users.noreply.github.com>
2026-01-26 02:02:27 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 9038.27 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,27 +1,12 @@
-from collections import deque
-
 def solution(bridge_length, weight, truck_weights):
-    answer = 0
-    
-    bridge = deque([0] * bridge_length)
-    truck_weights = deque(truck_weights)
-    bridge_weight = 0 
-    while(truck_weights):
-        answer += 1
-        bridge_weight -= bridge.popleft()
-        # bridge_weight = sum(bridge)
-        
-        
-        if (weight >= bridge_weight + truck_weights[0]): #sum함수에서 시간초과가 나니, 변수 설정
-            
-            pop = truck_weights.popleft()
-            bridge_weight += pop
-            bridge.append(pop)
-        else:
-            bridge.append(0)
-    
-    answer += bridge_length
-    
-    return answer
-
-
+    q=[0]*bridge_length
+    sec=0
+    while q:
+        sec+=1
+        q.pop(0)
+        if truck_weights:
+            if sum(q)+truck_weights[0]<=weight:
+                q.append(truck_weights.pop(0))
+            else:
+                q.append(0)
+    return sec
