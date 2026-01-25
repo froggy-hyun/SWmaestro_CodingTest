b6c19d13318f274e685e6dbc6660fa44762418ae
Evan Choi (최성현) <111727754+froggy-hyun@users.noreply.github.com>
2026-01-26 03:13:58 +0900

    [level 2] Title: 주식가격, Time: 84.87 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -3,7 +3,6 @@ from collections import deque
 def solution(prices):
     prices = deque(prices)
     answer = []
-    temp = 0
     while(prices):
         temp = prices.popleft()
 
@@ -12,8 +11,7 @@ def solution(prices):
             day += 1
             if temp > p:
                 break
-               
-        temp = 0
+              
         answer.append(day)
     
     return answer
